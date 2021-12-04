b86432342681738d7282798d316381e29c346908
mwada <wadamibass@gmail.com>
2021-12-04 14:21:46 +0900

    Herokuデプロイ

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Heroku用
+        if ($this->app->environment() == 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
